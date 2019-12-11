d98e1d5194fd006d7d36ad1bc144ce2bb10d732f
stevedole808 <stevedolelambda@gmail.com>
2019-12-11 06:54:39 -1000

    finished mvp

---
+++ my-app/src/components/Todo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export function Todo({todo, dispatch}) {
+  const toggleCompleted = () => {
+    dispatch({
+    type: 'TOGGLE_COMPLETED',
+    payload: todo.id
+    });
+  };
+
+  return (
+    <div onClick={toggleCompleted} className={`todo${todo.completed ? "completed" : ""}`} style={{ textDecoration: todo.completed ? "line-through" : ""}}>
+      <h3>{todo.item}</h3>
+    </div>
+  )
+}
---
+++ my-app/src/components/TodoForm.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+
+export const TodoForm = ({dispatch}) => {
+  const [item, setItem] = useState('');
+
+  const handleChanges = e => {
+    setItem(e.target.value);
+  };
+
+  const submitForm = e => {
+    e.preventDefault();
+    dispatch({
+      type: 'ADD_TODO',
+      payload: [item]
+    });
+  };
+
+  const clearCompleted = e => {
+    e.preventDefault();
+    dispatch({
+      type: 'CLEAR_COMPLETED'
+    });
+  };
+
+  return (
+    <div>
+      <form onSubmit={submitForm}>
+        <h1>Welcome To My Todo List</h1>
+        <input onChange={handleChanges} value={item} />
+        <button>Add Item</button>
+        <button onClick={clearCompleted}>Clear Item</button>
+      </form>
+    </div>
+  );
+}
